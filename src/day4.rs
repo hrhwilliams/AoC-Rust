4fe981e15e9edeea2453065d503ec52f759a342a
fiatlux20 <harlanrhwilliams@gmail.com>
2023-11-23 22:43:21 -0600

    day 4

---
+++ src/day4.rs
@@ -0,0 +1,48 @@
+use std::error::Error;
+
+const PROBLEM: &str = include_str!("day4.txt");
+
+fn parse_pair(pair: &str) -> (u32, u32) {
+    let dash = pair.find("-").expect("Missing '-'");
+    let lhs: u32 = pair[0..dash].parse::<u32>().expect("Failed to parse to int");
+    let rhs: u32 = pair[dash+1..].parse::<u32>().expect("Failed to parse to int");
+    (lhs, rhs)
+}
+
+pub fn solution1() -> Result<(), Box<dyn Error + 'static>> {
+    let answer: u32 = PROBLEM.split("\n").map(|line| {
+        let comma = line.find(",").expect("Missing ','");
+        let (first_lhs, first_rhs) = parse_pair(&line[0..comma]);
+        let (second_lhs, second_rhs) = parse_pair(&line[comma+1..]);
+
+        if first_lhs <= second_lhs && first_rhs >= second_rhs {
+            1
+        } else if second_lhs <= first_lhs && second_rhs >= first_rhs {
+            1
+        } else {
+            0
+        }
+    }).sum();
+
+    println!("Answer: {}", answer);
+
+    Ok(())
+}
+
+pub fn solution2() -> Result<(), Box<dyn Error + 'static>> {
+    let answer: u32 = PROBLEM.split("\n").map(|line| {
+        let comma = line.find(",").expect("Missing ','");
+        let (first_lhs, first_rhs) = parse_pair(&line[0..comma]);
+        let (second_lhs, second_rhs) = parse_pair(&line[comma+1..]);
+
+        if first_rhs < second_lhs || first_lhs > second_rhs{
+            0
+        } else {
+            1
+        }
+    }).sum();
+
+    println!("Answer: {}", answer);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+#![allow(dead_code)]
+
 use std::error::Error;
+
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() -> Result<(), Box<dyn Error + 'static>> {
-    day3::solution2()
+    day4::solution2()
 }
