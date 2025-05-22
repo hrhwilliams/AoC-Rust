9ba8612ea8a909218f78b2a678c8058765027e79
Harlan Williams <hrw@ku.edu>
2025-05-22 17:39:42 -0500

    start day10

---
+++ aoc-2015/src/day10.rs
@@ -0,0 +1,11 @@
+use std::error::Error;
+
+const PROBLEM: &str = "1113222113";
+
+pub fn solution1() -> Result<(), Box<dyn Error + 'static>> {
+    Ok(())
+}
+
+pub fn solution2() -> Result<(), Box<dyn Error + 'static>> {
+    Ok(())
+}
